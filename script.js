a987ad52b3c26da2211f468480ec91dd2f64722b
ssnamkeen <mrniteshmeena007@gmail.com>
2025-06-25 21:31:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function toggleLanguage() {
+  const lang = document.getElementById("langSelector").value;
+  document.querySelectorAll(".name").forEach(el => el.textContent = el.getAttribute("data-" + lang));
+  document.querySelectorAll(".price").forEach(el => el.textContent = el.getAttribute("data-" + lang));
+  document.querySelectorAll(".desc").forEach(el => el.textContent = el.getAttribute("data-" + lang));
+  document.querySelectorAll(".detailBtn").forEach(el => el.textContent = el.getAttribute("data-" + lang));
+  document.getElementById("title").textContent = lang === "hi" ? "एस.एस. नमकीन" : "S.S. Namkeen";
+}
